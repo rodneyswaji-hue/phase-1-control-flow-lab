0136e496abd7dde164231e8fd5f197789585a57b
Rodney Swaji <rodney.swaji@student.moringaschool.com>
2025-09-05 15:43:44 +0300

    control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
+  if (distance <= 400){
+    return "This one is on me!"
+  }else if ( distance > 400 && distance <= 2000 ){
+    return "That will be twenty bucks."
+  }
+   else if (distance > 2000 && distance <= 2500){
+    return "I will gladly take your thirty bucks."
+  // Write your code here!
+  }else return "No can do."
+    
+}
+
+function ternaryCheckCity(city){
+  
+  return city === "NYC"? "Ok, sounds good." : "No go.";
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case "generous":
+      return "Thank you so much."
+    case "not as generous":
+      return "Thank you."
+    default:
+      return "Bye."
+  }
   // Write your code here!
 }
